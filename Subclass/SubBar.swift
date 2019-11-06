c9af511671e71cfa181658094f180642a4f026ab
Andrej Šlegl <andrej.slegl@gmail.com>
2019-11-06 13:37:12 +0100

    Ported to swift: SubBar

---
+++ Subclass/SubBar.swift
@@ -0,0 +1,31 @@
+//
+//  SubBar.swift
+//  BeFit
+//
+//  Created by Andrej Slegl on 11/6/19.
+//
+
+import Foundation
+import Cocoa
+
+class SubBar: NSView {
+    private var lineColor = NSColor(calibratedRed: 0.42, green: 0.42, blue: 0.42, alpha: 1)
+    private var clickCount = 0
+    
+    override func mouseDown(with event: NSEvent) {
+        clickCount = (clickCount + 1) % 7
+        //let hue = CGFloat(clickCount) / 6
+        //lineColor = NSColor(calibratedHue: hue, saturation: 1, brightness: 1, alpha: 1)
+        super.mouseDown(with: event)
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        let wholeRect = bounds
+        let line = NSBezierPath()
+        line.move(to: NSPoint(x: 0, y: wholeRect.height))
+        line.line(to: NSPoint(x: wholeRect.width, y: wholeRect.height))
+        line.lineWidth = 1 // Make it easy to see
+        lineColor.set()
+        line.stroke()
+    }
+}
